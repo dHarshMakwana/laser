988e71fd385bdb71ba9830f7ec179978b21abe25
dharshmakwana <harshmakwana22211@gmail.com>
2024-02-09 11:30:42 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,247 @@
+"use client";
+import "aos/dist/aos.css";
+import AOS from "aos";
+import { useEffect } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+    setTimeout(() => {
+      AOS.refresh();
+    }, 1000);
+  }, []);
+
+  const apparatus = [
+    {
+      id: 1,
+      name: "Laser Light",
+      description: "Serves as the primary source of light for detection.",
+    },
+    { id: 2, name: "3.7v Battery", description: "Powers the entire system." },
+    {
+      id: 3,
+      name: "3x Mirror",
+      description: "Reflects the laser light to create a secure perimeter.",
+    },
+    {
+      id: 4,
+      name: "BC547 Transistor",
+      description: "Acts as an electronic switch or amplifier.",
+    },
+    {
+      id: 5,
+      name: "100k OHM",
+      description: "Manages voltage and current within the circuit.",
+    },
+    {
+      id: 6,
+      name: "Cardboard",
+      description:
+        "Provides a base structure for the installation of the system components.",
+    },
+    {
+      id: 6,
+      name: "Wires",
+      description:
+        "Establish connections and deliver power among all elements.",
+    },
+    {
+      id: 7,
+      name: "Buzzer",
+      description:
+        "Emits an audible alarm when the security system is breached.",
+    },
+    {
+      id: 8,
+      name: "5mm LDR",
+      description:
+        "Reacts to alterations in laser light intensity, triggering the alarm.",
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <>
+      <div className="flex flex-col items-center justify-center py-2 gradiant">
+        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center gap-28">
+          <section data-aos="fade-up" className="flex flex-col gap-8">
+            <h1 className="mt-6 text-9xl font-bold">Laser Security</h1>
+            <div className="flex gap-4 text-xl font-bold">
+              <div>presented by :</div>
+              <div className="flex gap-2">
+                <p>Harsh Poojary</p>
+                <p>Sahil Rewale</p>
+                <p>Harsh Makwana</p>
+                <p>Pratham Pawar</p>
+                <p>Atharva Puranik</p>
+                <p>Aryan More</p>
+              </div>
+            </div>
+          </section>
+          <section className="flex flex-wrap gap-8">
+            <p data-aos="fade-right" className="text-xl text-left md:w-1/2">
+              1. The purpose is to conceptualize and execute a Laser-Based
+              Security System designed to safeguard households from any
+              unauthorized intervention. An alarm will be activated when an
+              intrusion attempt is detected, enhancing the{` property's `}{" "}
+              security dimension. <br /> <br /> 2. Embark on the design and
+              establishment of a cost-effective laser system that optimizes
+              efficiency without compromising on its integral functionality.
+            </p>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              data-aos="fade-left"
+              src="/house.jpeg"
+              alt="laser"
+              width={500}
+              height={500}
             />
-          </a>
-        </div>
+          </section>
+          <section className="w-full">
+            <h2
+              data-aos="fade-right"
+              className="text-6xl text-left font-bold mb-8"
+            >
+              Utilization of Apparatus
+            </h2>
+            <div className="flex gap-6">
+              <div className="flex flex-col gap-6">
+                {apparatus.slice(0, 5).map((item, index) => (
+                  <div
+                    data-aos="zoom-in"
+                    key={index}
+                    className="flex gap-2 text-xl"
+                  >
+                    <span>{item.id}.</span>{" "}
+                    <h3 className="font-bold whitespace-nowrap">
+                      {item.name}:
+                    </h3>
+                    <p className="text-lg">{item.description}</p>
+                  </div>
+                ))}
+              </div>
+              <div className="flex flex-col gap-6">
+                {apparatus.slice(6, 9).map((item, index) => (
+                  <div
+                    data-aos="zoom-in"
+                    key={index}
+                    className="flex gap-2 text-xl"
+                  >
+                    <span>{item.id}.</span>{" "}
+                    <h3 className="font-bold whitespace-nowrap">
+                      {item.name}:
+                    </h3>
+                    <p className="text-lg">{item.description}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+          <section className="flex flex-col gap-8">
+            <h2
+              data-aos="fade-right"
+              className="text-6xl text-left font-bold mb-8"
+            >
+              Applications:
+            </h2>
+            <p data-aos="fade-left" className="text-xl text-left">
+              1. Home Security System: The designed laser-based system ensures
+              the security of residential properties by alerting owners of any
+              unauthorized intrusion attempts.
+              <br />
+              <br />
+              2. Large Property Security: The system can be effectively
+              implemented in oversized properties, such as military
+              installations, to ensure comprehensive coverage and heightened
+              security measures.
+            </p>
+          </section>
+          <section className="flex flex-col gap-8">
+            <h2
+              data-aos="fade-right"
+              className="text-6xl text-left font-bold mb-8"
+            >
+              Working principle
+            </h2>
+            <div className="flex gap-4 text-xl font-bold justify-between items-center">
+              <p data-aos="fade-right" className="text-left w-1/2">
+                This circuit consists of several components: an LDR (Light
+                Dependent Resistor) that changes its resistance based on light
+                intensity, a 100k ohm resistor to limit current and create a
+                voltage drop, a transistor <strong> {`"BC457" `}</strong> that
+                acts as a switch, and a buzzer. When exposed to sufficient
+                light, the LDR’s resistance decreases, causing Q1 to turn on and
+                activate the buzzer, producing an audible alert sound.
+              </p>
+              <Image
+                data-aos="fade-left"
+                src="/circuit.jpeg"
+                alt="laser"
+                width={500}
+                height={500}
+              />
+            </div>
+          </section>
+          <section className="flex flex-col gap-8">
+            <h2
+              data-aos="fade-right"
+              className="text-6xl text-left font-bold mb-8"
+            >
+              How LDR Works
+            </h2>
+            <div className="flex gap-4 text-xl font-bold justify-between items-center">
+              <p data-aos="fade-right" className="text-left w-1/2">
+                When the levels of light are high, the Light Dependent Resistor
+                (LDR) exhibits high resistance, obstructing current flow to the
+                base of the transistors. As a result, the buzzer remains silent
+                due to the restricted current flow.
+                <br />
+                <br />
+                Conversely, When the levels of light are low, its resistance
+                decreases, enabling the flow of current to the first{" "}
+                {`transistor's `}
+                base and subsequently to the second transistor. This movement of
+                current activates the buzzer.
+              </p>
+              <img
+                data-aos="fade-left"
+                src="/ldr.gif"
+                alt="laser"
+                width={500}
+                height={500}
+              />
+            </div>
+          </section>
+          <section>
+            <h2
+              data-aos="fade-right"
+              className="text-6xl text-left font-bold mb-8"
+            >
+              Operation
+            </h2>
+            <p data-aos="fade-right" className="text-xl text-left">
+              In its normal operational state, when {`there's `} no intruder,
+              the laser beam reflects off the mirrors and is received by the
+              Light Dependent Resistor (LDR). During this period, the resistance
+              of the LDR is low, which allows the current to flow through the
+              transistor. Resulting from this, the transistor remains turned off
+              and there is no sound from the buzzer. Any shift from this
+              normalcy, as by an intruder crossing the laser {`beam's `} path,
+              results in the interruption of the reflected light. This change
+              consequently triggers an increase in the {`LDR's `} resistance
+              which activates the transistor. Upon the{` transistor's `}{" "}
+              activation, the buzzer rings, alerting the property owner or the
+              security personnel of a potential intrusion.
+            </p>
+          </section>
+          <section className="h-[100vh]">
+            <h1 data-aos="fade-up" className="mt-6 text-9xl font-bold">
+              Thank you
+            </h1>
+          </section>
+        </main>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
